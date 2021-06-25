60b56b279826917334f4f562b55c2445ff4d26fa
AndreLps <andrelopes.spdia@gmail.com>
2021-06-25 09:56:42 -0300

    leitura para livro implementada

--- src/app/components/views/livro/livro-read/livro-read.component.ts
+++ src/app/components/views/livro/livro-read/livro-read.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Livro } from '../livro.model';
+import { LivroService } from '../livro.service';
 
 @Component({
   selector: 'app-livro-read',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LivroReadComponent implements OnInit {
 
-  constructor() { }
+  id_cat: String = ''
+
+  livro: Livro = {
+    id: '',
+    titulo: '',
+    nome_autor: '',
+    texto: ''
+  }
+
+  constructor(
+    private service: LivroService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.id_cat = this.route.snapshot.paramMap.get('id_cat')!
+    this.livro.id = this.route.snapshot.paramMap.get('id')!
+    this.findById();
+  }
+
+  update(): void {
+    this.service.update(this.livro).subscribe(resp => {
+      this.router.navigate([`categorias/${this.id_cat}/livros`])
+      this.service.mensagem("Livro atualizado com sucesso!")
+    }, err => {
+      this.router.navigate([`categorias/${this.id_cat}/livros`])
+      this.service.mensagem("Falha ao atualizar livro! Tente novamente mais tarde")
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate([`categorias/${this.id_cat}/livros`])
+  }
+
+  findById(): void {
+    this.service.findById(this.livro.id!).subscribe(resp => {
+      this.livro = resp;
+    })
   }
 
 }
